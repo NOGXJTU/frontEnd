dc13b403d8905e86b7530b0f4b648064a729a991
yan_sy <yan_sy@126.com>
2018-11-14 19:30:31 +0800

    complete navbar unit test
    use localVue
    
    add router to localVue
    
    add asyn test
    
    use factory function to get wrapper
    
    remove flushPromises

--- activate-fe/tests/unit/specs/navbar.spec.js
+++ activate-fe/tests/unit/specs/navbar.spec.js
@@ -1,34 +1,72 @@
 import { expect } from "chai";
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Navbar from "@/components/navbar.vue";
+import Vuex from "vuex";
+import VueRouter from "vue-router";
 import "../util";
 
-describe("navbar.vue", () => {
-  const mocks = {
-    $store: {
-      state: {
-        isLogged: false
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(VueRouter);
+
+function wrapperInstant(isLogged) {
+  return shallowMount(Navbar, {
+    localVue,
+    mocks: {
+      $store: {
+        state: {
+          isLogged: isLogged
+        }
       }
     }
-  };
-  const wrapper = shallowMount(Navbar, { mocks: mocks });
+  });
+}
 
-  it("show only login/register when not logged", () => {
+describe("navbar.vue", () => {
+  it("仅在未登录时显示登陆/注册", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="log"]').exists()).to.be.ok;
     expect(wrapper.find('[index="logout"]').exists()).to.not.be.ok;
   });
 
-  it("show only logout when logged in", () => {
-    const mocks = {
-      $store: {
-        state: {
-          isLogged: true
-        }
-      }
-    };
-    const wrapper = shallowMount(Navbar, { mocks: mocks });
-
+  it("仅在登陆后显示登出", () => {
+    const wrapper = wrapperInstant(true);
     expect(wrapper.find('[index="log"]').exists()).to.not.be.ok;
     expect(wrapper.find('[index="logout"]').exists()).to.be.ok;
+  });
+
+  it("显示主页", () => {
+    const wrapper = wrapperInstant(false);
+    expect(wrapper.find('[index="home"]').exists()).to.be.ok;
+  });
+
+  it("显示活动", () => {
+    const wrapper = wrapperInstant(false);
+    expect(wrapper.find('[index="activity"]').exists()).to.be.ok;
+  });
+
+  it("显示资源", () => {
+    const wrapper = wrapperInstant(false);
+    expect(wrapper.find('[index="resource"]').exists()).to.be.ok;
+  });
+
+  it.skip("显示组织", () => {
+    const wrapper = wrapperInstant(false);
+    expect(wrapper.find('[index="organization"]').exists()).to.be.ok;
+  });
+
+  it("显示个人中心", () => {
+    const wrapper = wrapperInstant(false);
+    expect(wrapper.find('[index="user"]').exists()).to.be.ok;
+  });
+
+  it("显示关于", () => {
+    const wrapper = wrapperInstant(false);
+    expect(wrapper.find('[index="about"]').exists()).to.be.ok;
+  });
+
+  it("显示章程", () => {
+    const wrapper = wrapperInstant(false);
+    expect(wrapper.find('[index="regulation"]').exists()).to.be.ok;
   });
 });
