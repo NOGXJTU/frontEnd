04143f268ed012192e525d395173e2ef513a5b3f
yan_sy <yan_sy@126.com>
2018-11-14 19:30:31 +0800

    complete navbar unit test

--- activate-fe/tests/unit/specs/navbar.spec.js
+++ activate-fe/tests/unit/specs/navbar.spec.js
@@ -13,12 +13,12 @@ describe("navbar.vue", () => {
   };
   const wrapper = shallowMount(Navbar, { mocks: mocks });
 
-  it("show only login/register when not logged", () => {
+  it("仅在未登录时显示登陆/注册", () => {
     expect(wrapper.find('[index="log"]').exists()).to.be.ok;
     expect(wrapper.find('[index="logout"]').exists()).to.not.be.ok;
   });
 
-  it("show only logout when logged in", () => {
+  it("仅在登陆后显示登出", () => {
     const mocks = {
       $store: {
         state: {
@@ -30,5 +30,33 @@ describe("navbar.vue", () => {
 
     expect(wrapper.find('[index="log"]').exists()).to.not.be.ok;
     expect(wrapper.find('[index="logout"]').exists()).to.be.ok;
+  });
+
+  it("显示主页", () => {
+    expect(wrapper.find('[index="home"]').exists()).to.be.ok;
+  });
+
+  it("显示活动", () => {
+    expect(wrapper.find('[index="activity"]').exists()).to.be.ok;
+  });
+
+  it("显示资源", () => {
+    expect(wrapper.find('[index="resource"]').exists()).to.be.ok;
+  });
+
+  it.skip("显示组织", () => {
+    expect(wrapper.find('[index="organization"]').exists()).to.be.ok;
+  });
+
+  it("显示个人中心", () => {
+    expect(wrapper.find('[index="user"]').exists()).to.be.ok;
+  });
+
+  it("显示关于", () => {
+    expect(wrapper.find('[index="about"]').exists()).to.be.ok;
+  });
+
+  it("显示章程", () => {
+    expect(wrapper.find('[index="regulation"]').exists()).to.be.ok;
   });
 });
