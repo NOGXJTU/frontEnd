fc219c735ab8642c53124fbcb100c7d89f8ecabb
yan_sy <yan_sy@126.com>
2018-11-15 17:30:19 +0800

    use factory function to get wrapper

--- activate-fe/tests/unit/specs/navbar.spec.js
+++ activate-fe/tests/unit/specs/navbar.spec.js
@@ -23,60 +23,54 @@ function wrapperInstant(isLogged) {
   });
 }
 
+describe("navbar.vue", () => {
   it("仅在未登录时显示登陆/注册", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="log"]').exists()).to.be.ok;
     expect(wrapper.find('[index="logout"]').exists()).to.not.be.ok;
   });
 
   it("仅在登陆后显示登出", () => {
-    const mocks = {
-      $store: {
-        state: {
-          isLogged: true
-        }
-      }
-    };
-    const wrapper = shallowMount(Navbar, { localVue, mocks });
+    const wrapper = wrapperInstant(true);
 
     expect(wrapper.find('[index="log"]').exists()).to.not.be.ok;
     expect(wrapper.find('[index="logout"]').exists()).to.be.ok;
   });
 
   it("显示主页", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="home"]').exists()).to.be.ok;
   });
 
   it("显示活动", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="activity"]').exists()).to.be.ok;
   });
 
   it("显示资源", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="resource"]').exists()).to.be.ok;
   });
 
-  it.skip("显示组织", () => {
+  it("显示组织", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="organization"]').exists()).to.be.ok;
   });
 
   it("显示个人中心", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="user"]').exists()).to.be.ok;
   });
 
   it("显示关于", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="about"]').exists()).to.be.ok;
   });
 
   it("显示章程", () => {
+    const wrapper = wrapperInstant(false);
     expect(wrapper.find('[index="regulation"]').exists()).to.be.ok;
   });
 
-  it("点击主页后正确跳转", () => {
-    const homeItem = wrapper.find('[index="home"]');
-    homeItem.trigger("click");
-    wrapper.vm.$nextTick(() => {
-      expect(wrapper.find(".intro-title").exists()).to.be.ok;
-      console.log(wrapper.vm.$route.path);
-      done();
-    });
-  });
+  it("点击登出后调用登出处理方法", () => {});
 });
